ba76581339e4c4e4a92dc07e3b0090468ee5cd88
Chinjuku <23048act@gmail.com>
2024-09-12 23:08:05 +0700

    ss

---
+++ cell04/rush00/js/index.js
@@ -0,0 +1,16 @@
+const box = document.getElementById('box');
+const image = document.getElementById('showcers');
+function showCertificate1(data) {
+    image.src = data
+    image.style.height = "600px"
+    box.style.height = "600px"
+    box.style.transition = ".5s"
+    image.style.transition = ".5s"
+}
+function showCertificate2(data) {
+    image.src = data
+    image.style.height = "600px"
+    box.style.height = "600px"
+    box.style.transition = ".5s"
+    image.style.transition = ".5s"
+}
